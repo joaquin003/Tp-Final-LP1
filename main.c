513250319afc1d4687414994a92928c26b3f5604
Joaquin Uliambre <joaquinuliambre.ju@gmail.com>
2022-11-17 21:30:28 -0300

    Implemento menu de acuerdo a la guia version final

--- main.c
+++ main.c
@@ -2,36 +2,105 @@
 
 int main()
 {
-    int decision = 0;
-    int x;
-    FILE *registro;
+    int decisionMenu = 0;
+    int modoLocal = 0;
+    int modoVisita = 0;
+    char modoActivo[100];
     char direccion[1000];
-    printf("ingrese la direccion y el nombre para el registro.txt\n----> ");
-    scanf("%s",direccion);
-    registro=fopen(direccion,"w");  //guarda el archivo en la direccion y con el nombre que el usuario desea
-    while (decision != 3)
-    {
-        printf("\nBienvenido al juego del HIP\n");
-        printf("Elija una opcion:\n");
-        printf("1- Ingresar como servidor\n");
-        printf("2- Ingresar como cliente\n");
-        printf("3- Salir\n");
-        scanf("%d", &decision);
+    FILE *registro;
+    // printf("ingrese la direccion y el nombre para el registro.txt\n----> ");
+    // scanf("%s", direccion);
+    // registro = fopen(direccion, "w"); // guarda el archivo en la direccion y con el nombre que el usuario desea
 
-        if (decision == 1)
+    while (decisionMenu != 5)
+    {
+        if (modoLocal && !modoVisita)
         {
-            socketServidor();
-            char mensaje[303];
-            printf("Ingrese el mensaje... \n ----> ");
-            scanf("%s",mensaje);
-            //char cadena[] = "[1];[cliente-grupo2-jugador2];[servidor-grupo1-jugador1];[Jugar];[activo];[1];[1];[3];[4];[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];#";
-            leer_mensaje(registro,mensaje);
+            strcpy(modoActivo, "Modo Local");
         }
-        else if (decision == 2)
+        else if (!modoLocal && modoVisita)
         {
-            socketCliente();
+            strcpy(modoActivo, "Modo Visita");
+        }
+        else
+        {
+            strcpy(modoActivo, "No configurado");
+        }
+
+        printf("\nID Grupo: 7            Jugador: No configurado\n");
+        printf("\nModo activado: %s\n\n", modoActivo);
+        printf("MENU DEL PROGRAMA\n\n");
+        printf("Opciones de navegacion:\n");
+        printf("1- Empezar partida\n");
+        printf("2- Configurar parametros\n");
+        printf("3- Autores\n");
+        printf("4- Ayuda\n");
+        printf("5- Salir\nElegir una opcion (1-5): ");
+        scanf("%d", &decisionMenu);
+
+        if (decisionMenu == 1) // empezar partida
+        {
+        }
+        else if (decisionMenu == 2) // configurar parametros
+        {
+            int decisionParametros = 0;
+
+            while (decisionParametros != 4)
+            {
+                printf("\nCONFIGURAR PARAMETROS\n\n");
+                printf("Opciones de navegacion:\n");
+                printf("1- Modo Local\n");
+                printf("2- Modo Visita\n");
+                printf("3- Directorio de archivos\n");
+                printf("4- Ir atras\nElegir una opcion(1-4): ");
+                scanf("%d", &decisionParametros);
+
+                if (decisionParametros == 1) // modo local
+                {
+                    printf("Cambiando a modo local...\n");
+                    modoLocal = 1;
+                    modoVisita = 0;
+                }
+                else if (decisionParametros == 2) // modo visita
+                {
+                    printf("Cambiando a modo visita...\n");
+                    modoVisita = 1;
+                    modoLocal = 0;
+                }
+                else if (decisionParametros == 3) // directorio de archivos
+                {
+                    printf("Ingrese el directorio de archivos de salida: ");
+                    scanf("%s", direccion);
+                }
+                else if (decisionParametros == 4) // ir atras
+                {
+                    printf("Volviendo a menu...\n");
+                }
+                else
+                {
+                    printf("\nEl valor ingresado es incorrecto, elija entre 1 al 4\n");
+                }
+            }
+        }
+        else if (decisionMenu == 3) // autores
+        {
+            printf("\nLos autores del programa son: \n");
+            printf("Joaquin Manuel Uliambre Frutos\n");
+            printf("Zinri Alice Bobabilla Peralta\n");
+        }
+        else if (decisionMenu == 4) // ayuda
+        {
+        }
+        else if (decisionMenu == 5) // salir
+        {
+            printf("Saliendo del programa...");
+        }
+        else
+        {
+            printf("\nEl valor ingresado es incorrecto, elija entre 1 al 5\n");
         }
     }
-    fclose(registro);
+
+    // fclose(registro);
     return 0;
 }
